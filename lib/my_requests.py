cffeac6c5dfc4adffb17676edaba3308eee594a6
eruvis <eruvis@mail.ru>
2022-02-17 19:38:51 +0700

    code from lesson 4.6

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -7,20 +7,20 @@ class MyRequests():
         return MyRequests._send(url, data, headers, cookies, 'POST')
 
     @staticmethod
-    def get(url: str, data: dict=None, headers: dict=None, cookies: dict=None):
+    def get(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
         return MyRequests._send(url, data, headers, cookies, 'GET')
 
     @staticmethod
-    def put(url: str, data: dict=None, headers: dict=None, cookies: dict=None):
+    def put(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
         return MyRequests._send(url, data, headers, cookies, 'PUT')
 
     @staticmethod
-    def delete(url: str, data: dict=None, headers: dict=None, cookies: dict=None):
+    def delete(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
         return MyRequests._send(url, data, headers, cookies, 'DELETE')
 
     @staticmethod
     def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
-        url = f"https//playground.learnqa.ru{url}"
+        url = f"https://playground.learnqa.ru/api{url}"
 
         if headers is None:
             headers = {}
@@ -30,11 +30,11 @@ class MyRequests():
         if method == "GET":
             response = requests.get(url, params=data, headers=headers, cookies=cookies)
         elif method == "POST":
-            response = requests.get(url, data=data, headers=headers, cookies=cookies)
+            response = requests.post(url, data=data, headers=headers, cookies=cookies)
         elif method == "PUT":
-            response = requests.get(url, data=data, headers=headers, cookies=cookies)
+            response = requests.put(url, data=data, headers=headers, cookies=cookies)
         elif method == "DELETE":
-            response = requests.get(url, data=data, headers=headers, cookies=cookies)
+            response = requests.delete(url, data=data, headers=headers, cookies=cookies)
         else:
             raise Exception(f"Bad HTTP method '{method}' was received")
 
