3184a1e0aa2a32ed34ca0498012cc08862f30874
eruvis <eruvis@mail.ru>
2022-02-17 11:56:50 +0700

    code from lesson 3.7

--- ex/ex6.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-print(f"Redirects: {response.history}")
-amount_redirects = len(response.history)
-print(f"Amount of redirects: {amount_redirects}")
-print(f"Final URL: {response.history[amount_redirects-1].url }")
