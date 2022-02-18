27c2e0596be7b8bb0af50df6535c9633b95f6541
Rustam Isaev <eruvis@mail.ru>
2022-02-18 16:40:56 +0700

    ex17

--- lib/assertions.py
+++ lib/assertions.py
@@ -13,6 +13,16 @@ class Assertions:
         assert response_as_dict[name] == expected_value, error_message
 
     @staticmethod
+    def assert_json_not_value_by_name(response: Response, name, value, error_message):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}"
+        assert response_as_dict[name] != value, error_message
+
+    @staticmethod
     def assert_json_has_key(response: Response, name):
         try:
             response_as_dict = response.json()
--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -52,3 +52,100 @@ class TestUserEdit(BaseCase):
             new_name,
             "Wrong name of the user after edit"
         )
+
+    def test_edit_user_being_unauthorized(self):
+        new_name = "Changed Name"
+
+        response = MyRequests.put(f"/user/26013", data={"firstName": new_name})
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode(
+            "utf-8") == "Auth token not supplied", f"Unexpected response content {response.content}"
+
+    def test_edit_user_auth_as_another_user(self):
+        # LOGIN
+        # ID 26029
+        data = {
+            'email': 'risaev@yahoo.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        # EDIT
+        new_username = "sparta"
+
+        MyRequests.put(f"/user/26000",
+                       headers={"x-csrf-token": token},
+                       cookies={"auth_sid": auth_sid},
+                       data={"firstName": new_username}
+                       )
+
+        # GET
+        response3 = MyRequests.get("/user/26000")
+
+        Assertions.assert_json_not_value_by_name(
+            response3,
+            "username", 
+            new_username,
+            "The username has changed. What shouldn't have been"
+        )
+
+    def test_edit_user_email(self):
+        # LOGIN
+        data = {
+            'email': 'risaev@yahoo.com',
+            'password': '1234'
+        }
+        user_id = 26029
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        # EDIT
+        new_email = (data['email']).replace('@', '')
+
+        response2 = MyRequests.put(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"email": new_email}
+                                   )
+
+        Assertions.assert_code_status(response2, 400)
+        assert response2.content.decode(
+            "utf-8") == f"Invalid email format", f"Unexpected response content {response2.content}"
+
+    def test_edit_user_name(self):
+        # LOGIN
+        data = {
+            'email': 'risaev@yahoo.com',
+            'password': '1234'
+        }
+        user_id = 26029
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        # EDIT
+        new_name = "a"
+
+        response2 = MyRequests.put(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_name}
+                                   )
+
+        Assertions.assert_code_status(response2, 400)
+        Assertions.assert_json_value_by_name(
+            response2,
+            "error",
+            "Too short value for field firstName",
+            f"Unexpected response content {response2.content}"
+        )
